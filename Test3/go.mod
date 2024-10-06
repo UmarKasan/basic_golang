4138595f773b333e018e81ecb444b693fdf5342f
Umar Kasan <umarksn@gmail.com>
2024-10-07 01:17:35 +0800

    v1

---
+++ Test3/go.mod
@@ -0,0 +1,5 @@
+module sdlc2
+
+go 1.22.4
+
+require github.com/veandco/go-sdl2 v0.5.0-alpha.6.0.20240902114001-f9a623944236
---
+++ Test3/test3.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		800, 600, sdl.WINDOW_SHOWN)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// defer calls when function exits
+	defer window.Destroy()
+
+	sdl.Delay(10000)
+}
